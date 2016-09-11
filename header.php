9f53680ae08cbb1734bd34642a5e5449550d71bf
Samuel <samuel.linehan15@gmail.com>
2016-09-11 11:45:44 -0400

    added store

--- header.php
+++ header.php
@@ -4,17 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title>Sam Linehan</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -30,8 +29,7 @@
     <nav class="blog-nav">
       <div class="container">
         <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">Store</a>
+          <?php wp_list_pages('&title_li='); ?>
         </nav>
       </div>
     </nav>
---
+++ woocommerce.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="row">
+  <div class="col-sm-12">
+    
+    <?php woocommerce_content(); ?>
+
+  </div>
+
+</div>
+
+<?php get_footer(); ?>
